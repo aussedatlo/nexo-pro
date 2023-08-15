035e40b0d9a01de5367da2c6a9e9c63559bcadd8
Louis Aussedat <aussedat.louis@gmail.com>
2023-08-15 22:45:38 +0200

    jest config: format file

--- jest.config.js
+++ jest.config.js
@@ -3,26 +3,24 @@ module.exports = {
   rootDir: './',
   globals: {
     __DEV__: true,
-    __PROD__: false
+    __PROD__: false,
   },
   testEnvironment: 'node',
-  preset: "ts-jest",
+  preset: 'ts-jest',
   verbose: true, // report individual test
   bail: false, // enable to stop test when an error occur,
   detectOpenHandles: false,
   moduleDirectories: ['node_modules', 'src', 'test'],
   testMatch: ['**/test/**/*.test.ts?(x)'],
-  testPathIgnorePatterns: ['node_modules/', 'dist/', '.json'],
-  collectCoverageFrom: [
-    'src/**/*.ts'
-  ],
+  testPathIgnorePatterns: ['node_modules/', 'lib/', '.json'],
+  collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
     // coverage strategy
     global: {
       branches: 80,
       functions: 80,
       lines: 50,
-      statements: -10
-    }
-  }
+      statements: -10,
+    },
+  },
 };
